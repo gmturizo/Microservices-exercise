0a0d2136826f9dcb4dc06726a93704bc5669a8c6
gmturizo <gmturizo@bancolombia.com.co>
2026-06-24 09:35:37 -0500

    feat: estructura inicial multi-modulo con 3 microservicios

---
+++ service-flota/src/test/java/co/com/bancolombia/flota_service/FlotaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.bancolombia.flota_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlotaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
